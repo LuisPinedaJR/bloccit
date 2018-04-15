87a9492d971a6d5b4c8f32f1950291786a02b107
Luis Pineda JR <lspnd00@gmail.com>
2018-04-15 04:32:33 -0400

    more crud assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ require 'random_data'
   )
 end
 posts = Post.all
-
+#create comments
 100.times do
   Comment.create!(
     post: posts.sample,
@@ -18,6 +18,16 @@ end
  post = Post.find_or_create_by(title: "A unique title", body: "A unique body")
  Comment.find_or_create_by(post: post, body: "A unique comment")
 
+#create Questions
+100.times do
+  Question.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    resolved: false
+  )
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
