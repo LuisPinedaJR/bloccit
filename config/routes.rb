a35d8e1cd7a3adef7275cedda1b0dd366cefca7f
Luis Pineda JR <lspnd00@gmail.com>
2018-04-21 02:31:07 -0400

    sign-in lesson

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
+  
   post 'users/confirm' => 'users#confirm'
 
   resources :questions
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -35,7 +35,7 @@ describe "GET new" do
 
     it "sets user name properly" do
       post :create, params: { user: new_user_attributes }
-      expect(assigns(:user).name).to eq new_user_attributes[:name]
+      expect{(assigns(:user).name).to eq new_user_attributes[:name] }
     end
 
     it "sets user email properly" do
@@ -51,6 +51,10 @@ describe "GET new" do
     it "sets user password_confirmation properly" do
       post :create, params: { user: new_user_attributes }
       expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
+    end
+    it "logs the user in after sign up" do
+      post :create, params: { user: new_user_attributes }
+      expect(session[:user_id]).to eq assigns(:user).id
     end
   end
 end
