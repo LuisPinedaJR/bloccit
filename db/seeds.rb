b35becd8247037592d555cfd3f2ace3a36cc03f4
Luis Pineda JR <lspnd00@gmail.com>
2018-05-05 20:59:08 -0400

    voting lesson

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,13 +31,19 @@ end
 
 # create posts
 50.times do
-  Post.create!(
+  post = Post.create!(
     user:   users.sample,
     topic:  topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
+  
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+
 end
+
+
 posts = Post.all
 
 #create comments
@@ -90,5 +96,6 @@ puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
 puts "#{Advertisement.count} advertisements created"
 puts "#{Question.count} questions created"
